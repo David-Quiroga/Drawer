13a3dbe7993ca99e6e6675d1991f43fe28c71d7f
David Quiroga <97131802+David-Quiroga@users.noreply.github.com>
2023-11-24 21:20:42 -0500

    Dropdown

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:drawer/src/drawer.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // ignore: prefer_const_constructors
+      home: MenuLateral(),
+    );
+  }
+}
